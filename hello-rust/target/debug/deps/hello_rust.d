7d1a1ab978599901fea7d4a85ee31012bb2c3e53
Vanessa <50547897+vhxma@users.noreply.github.com>
2022-09-07 13:33:17 +0800

    update learn-rust

---
+++ hello-rust/target/debug/deps/hello_rust.d
@@ -0,0 +1,5 @@
+C:\Users\mavanessa\Documents\GitHub\learn-rust\hello-rust\target\debug\deps\hello_rust.exe: src\main.rs
+
+C:\Users\mavanessa\Documents\GitHub\learn-rust\hello-rust\target\debug\deps\hello_rust.d: src\main.rs
+
+src\main.rs:
